7d8e3e6e4e71058153b1965bb5b401ab3fbc4c59
Prejith Premkumar <prajithpremg@gmail.com>
2018-02-14 06:55:21 +0530

    Change bg colour, add fonts, random theming

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const hours = date.getHours();
+
+if(hours > 12) {
+    //TODO: Change style at night time and day time accordingly 
+}
+
+//TODO: Change colour of circular progress bar depending on its content
